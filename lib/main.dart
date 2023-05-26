6c731d9be040db5e717af01f5450338719d585dc
David Michael II <knowdavidmichael@gmail.com>
2023-05-26 19:26:52 +0100

    set app orientation to only portrait up

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pureair_v2/app/app.dart';
 
 import 'injector/injector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  
   configureDependencies();
 
   runApp(const PureAirApp());
