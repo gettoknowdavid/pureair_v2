b140bd2937f0bfbdcde150e34265e3078c8eee7a
David Michael II <knowdavidmichael@gmail.com>
2023-05-26 19:26:31 +0100

    add responsive_framework for responsiveness

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 import 'router/router.dart';
 
@@ -25,6 +26,15 @@ class _PureAirAppState extends State<PureAirApp> {
           TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
           TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
         }),
+      ),
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child!,
+        breakpoints: [
+          const Breakpoint(start: 0, end: 450, name: MOBILE),
+          const Breakpoint(start: 451, end: 800, name: TABLET),
+          const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
       ),
     );
   }
