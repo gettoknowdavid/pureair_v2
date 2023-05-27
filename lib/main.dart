140d1e80354466ba2e5987b50b8d9937b7c2418f
David Michael II <knowdavidmichael@gmail.com>
2023-05-27 04:02:09 +0100

    add bloc providers to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pureair_v2/app/app.dart';
+import 'package:pureair_v2/application/application.dart';
 
 import 'injector/injector.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  
+
   configureDependencies();
 
-  runApp(const PureAirApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (ctx) => di<LoginCubit>(), lazy: true),
+        BlocProvider(create: (ctx) => di<RegisterCubit>(), lazy: true),
+        BlocProvider(create: (ctx) => di<ForgotPasswordCubit>(), lazy: true),
+        BlocProvider(create: (ctx) => di<AuthBloc>()),
+      ],
+      child: const PureAirApp(),
+    ),
+  );
 }
