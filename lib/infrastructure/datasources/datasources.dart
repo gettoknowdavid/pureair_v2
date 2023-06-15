87155204822932f65e5ea2e1ba17b1c3742b34bd
David Michael II <knowdavidmichael@gmail.com>
2023-06-15 11:50:43 +0100

    add weather remote datasource linking to OPenWeather API

--- lib/infrastructure/datasources/datasources.dart
+++ lib/infrastructure/datasources/datasources.dart
@@ -1,3 +1,4 @@
 export 'aqi/air_quality_local_datasource.dart';
 export 'aqi/air_quality_remote_datasource.dart';
 export 'auth_local_datasource.dart';
+export 'weather/index.dart';
---
+++ lib/infrastructure/datasources/weather/weather_remote_datasource.dart
@@ -0,0 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/retrofit.dart';
+
+import '../../infrastructure.dart';
+
+part 'weather_remote_datasource.g.dart';
+
+/// Remote data source for retrieving weather information from a REST API.
+
+@RestApi()
+abstract class WeatherRemoteDatasource {
+  /// Constructs an instance of [WeatherRemoteDatasource].
+  @factoryMethod
+  factory WeatherRemoteDatasource(Dio dio, {String baseUrl}) =
+      _WeatherRemoteDatasource;
+
+  /// Retrieves the current air quality information by latitude and longitude.
+  ///
+  /// `lat`: The latitude.
+  ///
+  /// `lon`: The longitude.
+  ///
+  /// `exclude`: By using this parameter you can exclude some parts of the
+  /// weather data from the API response. It should be a comma-delimited
+  /// list (without spaces). Available values:
+  ///  *  current
+  ///  *  minutely
+  ///  *  hourly
+  ///  *  daily
+  ///  *  alerts
+  @GET('/data/3.0/onecall')
+  Future<WeatherDto> getWeather({
+    @Query('lat') required double lat,
+    @Query('lon') required double lon,
+    @Query('exclude') String? exclude,
+    @Query('units') String? units,
+  });
+}
