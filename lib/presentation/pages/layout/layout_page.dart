511754e769e59d30c8fc54e1c4baee7f20ec723d
David Michael II <knowdavidmichael@gmail.com>
2023-07-04 01:57:25 +0100

    feat: add avatar to profile page

--- lib/presentation/pages/layout/layout_page.dart
+++ lib/presentation/pages/layout/layout_page.dart
@@ -1,27 +1,18 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
-import 'package:pureair_v2/application/application.dart';
 import 'package:pureair_v2/config/router/router.dart';
 import 'package:pureair_v2/constants/constants.dart';
 
 import 'widgets/widgets.dart';
 
 @RoutePage()
-class LayoutPage extends HookWidget {
+class LayoutPage extends StatelessWidget {
   const LayoutPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     final appBarStyle = Theme.of(context).textTheme.bodyLarge;
-    useEffect(() {
-      WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-        await context.read<MapCubit>().initialized();
-      });
-      return null;
-    }, []);
 
     return AutoTabsScaffold(
       routes: const [
@@ -41,6 +32,8 @@ class LayoutPage extends HookWidget {
             break;
           case 2:
             child = AppBar(title: Text(kRankTitle, style: appBarStyle));
+          case 3:
+            child = const ProfileAppBar();
           default:
         }
 
--- lib/presentation/pages/layout/widgets/widgets.dart
+++ lib/presentation/pages/layout/widgets/widgets.dart
@@ -1,2 +1,3 @@
 export 'app_bottom_navigation_bar.dart';
 export 'home_app_bar.dart';
+export 'profile_app_bar.dart';
