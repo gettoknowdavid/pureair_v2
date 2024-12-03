4df6ad7c67ef7446ea48ceb7208207d2125e86f9
David Michael II <knowdavidmichael@gmail.com>
2024-12-04 00:44:21 +0100

    add placeholder for home app bar

--- lib/src/features/auth/presentation/pages/home_page.dart
+++ lib/src/features/auth/presentation/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:pureair_v2/src/features/auth/application/application.dart';
 
 class HomePage extends ConsumerWidget {
@@ -9,6 +10,7 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final user = ref.watch(userProvider);
     return Scaffold(
+      appBar: const HomeAppBar(),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -22,6 +24,53 @@ class HomePage extends ConsumerWidget {
           ],
         ),
       ),
+    );
+  }
+}
+
+class HomeAppBar extends ConsumerWidget implements PreferredSizeWidget {
+  const HomeAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18),
+        child: Row(
+          children: [
+            UserAvatar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class UserAvatar extends ConsumerWidget {
+  const UserAvatar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final colors = Theme.of(context).colorScheme;
+    final imageUrl = ref.watch(userProvider.select((value) => value?.imageUrl));
+    return RawMaterialButton(
+      onPressed: () {},
+      elevation: 12,
+      constraints: const BoxConstraints(
+        maxHeight: 48,
+        minHeight: 48,
+        maxWidth: 48,
+        minWidth: 48,
+      ),
+      shape: RoundedRectangleBorder(
+        side: BorderSide(width: 4, color: colors.onPrimary),
+      ),
+      child: imageUrl == null
+          ? const Icon(PhosphorIconsBold.user)
+          : const SizedBox(),
     );
   }
 }
